db0691f62a69c5e5f5c8476ce86fd8d5681a9ca9
Nqabayethu Mlotshwa <nqabayethumlotshwa@gmail.com>
2025-04-02 17:42:33 +0200

    Initial commit

---
+++ src/main/java/sia/taco_cloud/OrderController.java
@@ -0,0 +1,30 @@
+package sia.taco_cloud;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import lombok.extern.slf4j.Slf4j;
+import sia.taco_cloud.TacoOrder;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+ @GetMapping("/Current")
+ public String orderForm()
+ {
+    return "orderForm";
+ }  
+
+ @PostMapping
+ public String processOrder(TacoOrder order, SessionStatus sessionStatus){
+    log.info("Order Submitted: {}", order);
+    sessionStatus.setComplete();
+    return "redirect :/";
+ }
+    
+}
